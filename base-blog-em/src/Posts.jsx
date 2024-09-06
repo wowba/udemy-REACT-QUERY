45c436710f6060387717ed134e58e73e90d2753b
이영욱 <87873821+wowba@users.noreply.github.com>
2024-09-06 11:00:13 +0900

    useQuery를 이용해 데이터 받아오기

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
@@ -8,8 +9,24 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  // useQuery는 다양한 값을 받는다.
+  // isLoading과 isFetching 차이
+  // - isFetching
+  // - 비동기 쿼리가 아직 해결되지 않음 -> 페이징 등을 사용할 때 중요함.
+  // - isLoading
+  // - 캐시된 데이터도 없고, 비동기 쿼리도 해결되지 않음.
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["posts"], // 쿼리키는 항상 배열로 받는다, v4 이상에서!
+    queryFn: fetchPosts, // 데이터를 받아오기 위해 실행할 함수
+  });
+
+  if (isLoading) {
+    return <h3>loading</h3>;
+  }
+
+  if (isError) {
+    return <h3> error </h3>;
+  }
 
   return (
     <>
