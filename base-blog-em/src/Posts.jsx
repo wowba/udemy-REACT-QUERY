9e497a21fa723a0cfa1088fd96600b02ce56f927
이영욱 <87873821+wowba@users.noreply.github.com>
2024-10-14 10:39:23 +0900

    Posts 페이지네이션 추가

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -3,10 +3,14 @@ import { useQuery } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
-const maxPostPage = 10;
+
+const MAX_POST_PAGE = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const goNextPage = () => setCurrentPage((prev) => prev + 1);
+  const goPrevPage = () => setCurrentPage((prev) => prev - 1);
+
   const [selectedPost, setSelectedPost] = useState(null);
 
   // useQuery는 다양한 값을 받는다.
@@ -16,8 +20,8 @@ export function Posts() {
   // - isLoading
   // - 캐시된 데이터도 없고, 비동기 쿼리도 해결되지 않음.
   const { data, isLoading, isError } = useQuery({
-    queryKey: ["posts"], // 쿼리키는 항상 배열로 받는다, v4 이상에서!
-    queryFn: fetchPosts, // 데이터를 받아오기 위해 실행할 함수
+    queryKey: ["posts", currentPage], // 쿼리키는 항상 배열로 받는다, v4 이상에서!
+    queryFn: () => fetchPosts(currentPage), // 데이터를 받아오기 위해 실행할 함수
     staleTime: 5000, // staleTime의 기본값은 0
   });
 
@@ -43,11 +47,11 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <= 1} onClick={goPrevPage}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage >= MAX_POST_PAGE} onClick={goNextPage}>
           Next page
         </button>
       </div>
