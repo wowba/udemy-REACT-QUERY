769408a7bed001829f1cee8fa1bfa623b7b04165
이영욱 <87873821+wowba@users.noreply.github.com>
2024-10-21 16:09:57 +0900

    useTreatment 커스텀 훅 작성

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
 import type { Treatment } from "@shared/types";
 
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get("/treatments");
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  // 데이터 fetch 전 기본 반환값을 설정할 수 있다.
+  const fallback: Treatment[] = [];
+
+  const { data = fallback } = useQuery({
+    queryKey: [queryKeys.treatments],
+    queryFn: getTreatments,
+  });
+
+  return data;
 }
