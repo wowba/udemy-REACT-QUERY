5fcb6d16d224957b297ce10c31a83a3b687daaae
이영욱 <87873821+wowba@users.noreply.github.com>
2024-10-22 16:01:18 +0900

    useStaff 커스텀 훅 작성

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 import type { Staff } from "@shared/types";
@@ -8,17 +9,20 @@ import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 
 // query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get("/staff");
+  return data;
+}
 
 export function useStaff() {
   // for filtering staff by treatment
   const [filter, setFilter] = useState("all");
 
-  // TODO: get data from server via useQuery
-  const staff: Staff[] = [];
+  const fallback: Staff[] = [];
+  const { data: staff = fallback } = useQuery({
+    queryKey: [queryKeys.staff],
+    queryFn: getStaff,
+  });
 
   return { staff, filter, setFilter };
 }
