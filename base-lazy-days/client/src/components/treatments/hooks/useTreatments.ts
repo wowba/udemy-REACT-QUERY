c7dba913c9187a5d52e016fcc0ec7488e643755f
wowba <leeyuwk54@gmail.com>
2024-10-24 22:38:35 +0900

    usePrefetchTreatments 훅 작성

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import type { Treatment } from "@shared/types";
 
@@ -21,4 +21,12 @@ export function useTreatments(): Treatment[] {
   });
 
   return data;
+}
+
+export function usePrefetchTreatments(): void {
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery({
+    queryKey: [queryKeys.treatments],
+    queryFn: getTreatments,
+  });
 }
