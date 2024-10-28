25de723c6f14af2dddcb791f925c1aa5dc52328b
이영욱 <87873821+wowba@users.noreply.github.com>
2024-10-28 13:04:41 +0900

    refetchInterval 추가

--- base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -84,6 +84,7 @@ export function useAppointments() {
     queryFn: () => getAppointments(monthYear.year, monthYear.month),
     select: (data) => selectFn(data, showAll),
     refetchOnWindowFocus: true,
+    refetchInterval: 60000,
     ...commonQueryOption,
   });
 
