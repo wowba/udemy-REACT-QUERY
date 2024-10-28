b9a59669311bae33ffbf351c7993281cd0f14330
이영욱 <87873821+wowba@users.noreply.github.com>
2024-10-28 11:07:12 +0900

    useStaff 훅 select 함수 작성

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import type { Staff } from "@shared/types";
 
@@ -18,10 +18,22 @@ export function useStaff() {
   // for filtering staff by treatment
   const [filter, setFilter] = useState("all");
 
+  const selectFn = useCallback(
+    (data: Staff[]) => {
+      if (filter === "all") {
+        return data;
+      } else {
+        return filterByTreatment(data, filter);
+      }
+    },
+    [filter]
+  );
+
   const fallback: Staff[] = [];
   const { data: staff = fallback } = useQuery({
     queryKey: [queryKeys.staff],
     queryFn: getStaff,
+    select: (data) => selectFn(data),
   });
 
   return { staff, filter, setFilter };
